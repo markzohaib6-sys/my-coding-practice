7f1078e52340312c5c56ed6722663f82d2d816d3
Zohaib <markzohaib6@gmail.com>
2026-09-07 14:31:09 +0530

    updated files

---
+++ python practice/dict.py
@@ -0,0 +1,4 @@
+person={'name':"Zohaib",'age':21,'courses':['maths','science','physics']}
+
+for i in person.items():
+    print(i)
---
+++ python practice/list.py
@@ -0,0 +1,9 @@
+course=['PHYSICS','MATHS','CHEMISTRY','SCIENCE']
+course_2=['PYTHON','CN','DSA','IOT']
+
+for index,item in enumerate (course,start=1):
+    print(index,item)
+
+print("----------------------------------")
+for numbering,mtrl in enumerate (course_2,start=1):
+    print(numbering,mtrl)
