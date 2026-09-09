176d60a3b8f61b0bc46676ffab0f7d0644281bd0
Zohaib <markzohaib6@gmail.com>
2026-09-09 14:51:31 +0530

    updated files

---
+++ python practice/functions.py
@@ -0,0 +1,17 @@
+def name(*args,**kwargs):
+    print("args:",args)
+    print("kwargs:",kwargs)
+
+courses=['maths','python']
+student={'name':"Zohaib",'age':21}
+
+name(*courses,**student)
+
+
+
+
+  
+
+
+
+    
